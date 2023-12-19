22bd2bd700c31016f6aa4899adb0e6dc097b27c2
user <user@192.168.219.107>
2023-12-19 14:36:06 +0900

    팀장
    컨트롤러 - 캘린더 서블릿 파일 추가
    모델 - VO 추가
    웹앱 - 풀캘린더 폴더 추가, calendar/index html파일 추가(캘린더 테스트용)

--- Energizo_Project/src/main/java/com/todaysmenu/controller/calServlet.java
+++ Energizo_Project/src/main/java/com/todaysmenu/controller/calServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/cal.do")
+
 public class calServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
