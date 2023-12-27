b15ddbe6b06fe8318fa8ff32821d32bde4434ce3
user <user@192.168.219.107>
2023-12-27 19:36:16 +0900

    팀장 - 유튜브 vo dao 추가 / mapper 코드 추가

---
+++ EP2/src/main/java/com/todaysmenu/db/YoutubeDAO.java
@@ -0,0 +1,23 @@
+package com.todaysmenu.db;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import com.todaysmenu.model.YoutubeVO;
+
+public class YoutubeDAO {
+	
+	private SqlSessionFactory factory = SqlSessionManager.getFactory();
+	
+	
+	public List<YoutubeVO> showyoutube(String youtube_name) {
+		
+		SqlSession session = factory.openSession(true);
+		List<YoutubeVO> y_list = session.selectList("showyoutube" ,youtube_name);
+		
+		return y_list;
+		
+	}
+}
