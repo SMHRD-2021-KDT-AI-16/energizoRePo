beadb3be5aaf81e4edab5c573f44dd9ed32bc628
user <user@192.168.219.110>
2023-12-27 09:23:56 +0900

    지승 - ai답변 필터링해줄 필터링 서비스 구현

--- EP2/src/main/java/com/todaysmenu/model/MealVO.java
+++ EP2/src/main/java/com/todaysmenu/model/MealVO.java
@@ -2,39 +2,29 @@ package com.todaysmenu.model;
 
 public class MealVO {
 	
-	private String meal_id;
-	private String meal_time;
-	private String food_name;
+	private String User_id;
+	private String meal_type;
+	private String meal_menu;
 	
-	public MealVO(String meal_id, String meal_time, String food_name) {
-		this.meal_id = meal_id;
-		this.meal_time = meal_time;
-		this.food_name = food_name;
+	public String getUser_id() {
+		return User_id;
 	}
-
-	public String getMeal_id() {
-		return meal_id;
+	public void setUser_id(String user_id) {
+		User_id = user_id;
 	}
-
-	public void setMeal_id(String meal_id) {
-		this.meal_id = meal_id;
+	public String getMeal_type() {
+		return meal_type;
 	}
-
-	public String getMeal_time() {
-		return meal_time;
+	public void setMeal_type(String meal_type) {
+		this.meal_type = meal_type;
 	}
-
-	public void setMeal_time(String meal_time) {
-		this.meal_time = meal_time;
+	public String getMeal_menu() {
+		return meal_menu;
 	}
-
-	public String getFood_name() {
-		return food_name;
+	public void setMeal_menu(String meal_menu) {
+		this.meal_menu = meal_menu;
 	}
-
-	public void setFood_name(String food_name) {
-		this.food_name = food_name;
-	}
+	
 	
 	
 	
